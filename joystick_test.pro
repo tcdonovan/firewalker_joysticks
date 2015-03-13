5ad7f992c7da6ddf0fa0c4aa36b73f47f74f5b6c
root <root@Firewalker.sdsmt.local>
2015-03-13 12:51:48 -0600

    First commit

---
+++ joystick_test.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-10T11:31:29
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = joystick_test
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    js0.cpp \
+    js1.cpp
+
+HEADERS += \
+    js0.h \
+    js1.h
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <QCoreApplication>
+#include <QThread>
+
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/joystick.h>
+
+#include "js0.h"
+#include "js1.h"
+
+#define JOY_DEV0 "/dev/input/js0"
+#define JOY_DEV1 "/dev/input/js1"
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+
+    QThread js0Thread;
+    QThread js1Thread;
+
+    JS0* myJS0 = new JS0();
+    JS1* myJS1 = new JS1();
+
+    myJS0->moveToThread(&js0Thread);
+    myJS1->moveToThread(&js1Thread);
+
+    QObject::connect(&js0Thread,SIGNAL(started()),myJS0,SLOT(doWork0()));
+    QObject::connect(&js1Thread,SIGNAL(started()),myJS1,SLOT(doWork1()));
+
+
+    js0Thread.start();
+    js1Thread.start();
+
+
+
+    return a.exec();
+}
+
